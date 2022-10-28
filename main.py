437543f82cbe81bebbdcb0f7a30d3e00fa5027eb
joeychuavertex <joey.chua@vertexholdings.com>
2022-10-28 17:05:15 +0800

    Data cleaning and count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Preqin, Tracxn, Pitchbook Deduplication")
+
 pitchbook = pd.read_csv("pitchbook.csv")
 pitchbook['source'] = 'pitchbook'
 pitchbook = pitchbook[['Companies', 'source', 'Last Financing Date', 'Last Financing Deal Type', 'HQ Location']]
@@ -11,7 +13,26 @@ pitchbook.rename(columns={
     "Last Financing Deal Type": "Last Financing Deal Type",
     "HQ Location": "Location"
 }, inplace=True)
+pitchbook['Location'] = pitchbook['Location'].astype(str)
+pitchbook = pitchbook[~pitchbook['Location'].str.contains('|'.join([
+    'China',
+    'CA',
+    'MA',
+    'WA',
+    'WY',
+    'DE',
+    'Estonia',
+    'Georgia',
+    'NY',
+    'British Virgin Islands',
+    'Tajikistan',
+    'CO',
+    'NJ',
+    'Seychelles',
+]))]
+pitchbook_count = len(pitchbook)
 st.header("Pitchbook")
+st.metric(label="Count", value=pitchbook_count)
 st.dataframe(pitchbook)
 
 tracxn = pd.read_csv("tracxn.csv")
@@ -24,7 +45,10 @@ tracxn.rename(columns={
     "Company Stage": "Last Financing Deal Type",
     "Country": "Location"
 }, inplace=True)
+tracxn = tracxn.drop_duplicates()
+tracxn_count = len(tracxn)
 st.header("Tracxn")
+st.metric(label="Count", value=tracxn_count)
 st.dataframe(tracxn)
 
 preqin = pd.read_csv("preqin.csv")
@@ -37,10 +61,25 @@ preqin.rename(columns={
     "MOST RECENT DEAL TYPE/STAGE": "Last Financing Deal Type",
     "COUNTRY": "Location"
 }, inplace=True)
+preqin_count = len(preqin)
 st.header("Preqin")
+st.metric(label="Count", value=preqin_count)
 st.dataframe(preqin)
 
 data = pd.concat([pitchbook, tracxn, preqin])
 data.sort_values('Company', ascending=True, inplace=True)
+combined_count = len(data)
 st.header("Combined")
+st.metric(label="Count", value=combined_count)
 st.dataframe(data)
+
+
+st.header("Same Companies")
+data_same = pd.concat([pitchbook, tracxn, preqin])
+
+data_exact = data_same[data_same.duplicated('Company', keep=False)]
+data_exact.sort_values('Company', ascending=True, inplace=True)
+st.header("Combined")
+samedata_count = len(data_exact)
+st.metric(label="Count", value=samedata_count)
+st.dataframe(data_exact)
